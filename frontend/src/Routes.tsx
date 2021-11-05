1a5e61b924eac92872c9d58abf3baf857a0518fd
Rafael Alexandre <rafaelxndre@gmail.com>
2021-11-05 17:45:42 -0300

    First request

--- frontend/src/Routes.tsx
+++ frontend/src/Routes.tsx
@@ -9,7 +9,7 @@ const Routes = () => {
             
                 <Route path="/" exact>
                     <Home/>
-                </Route>                  
+                </Route>               
                 
 
                 <Route path="/dashboard">
